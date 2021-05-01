497872fc3a68220ec0141e78574b25f69e9f06c9
Lamardinho <slezkin23@gmail.com>
2021-05-01 21:44:39 +0500

    start

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,9 +17,25 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springdoc:springdoc-openapi-kotlin:1.5.1")
+    implementation("org.springdoc:springdoc-openapi-ui:1.5.1")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("com.vladmihalcea:hibernate-types-52:2.2.2")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("com.auth0:java-jwt:3.10.3")
+
+    implementation("io.swagger.core.v3:swagger-annotations:2.1.6")
+
+    implementation("org.postgresql:postgresql:42.2.9")
+    implementation("org.liquibase:liquibase-core")
+    testImplementation("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
 }
 
 tasks.withType<KotlinCompile> {
