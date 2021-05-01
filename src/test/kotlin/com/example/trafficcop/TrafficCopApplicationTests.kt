62a79f41dc532fcd5c677082204616d428e9cd14
Lamardinho <slezkin23@gmail.com>
2021-05-01 21:26:40 +0500

    start

---
+++ src/test/kotlin/com/example/trafficcop/TrafficCopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.trafficcop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TrafficCopApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
